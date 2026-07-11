631e8c415f29accca8ebdb3390526bd62a831ea1
Isabella Le <isabellale@Isabellas-Air-2.gateway>
2026-07-11 17:24:08 +1000

    chore: add trackName to Round and ReadBook

---
+++ prisma/migrations/20260710220000_add_track_name/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ReadBook" ADD COLUMN     "trackName" TEXT NOT NULL DEFAULT 'General';
+
+-- AlterTable
+ALTER TABLE "Round" ADD COLUMN     "trackName" TEXT NOT NULL DEFAULT 'General';
